fc3834629ce56e8b4ff938bbf9b885754a5e60cb
Stowy <stowy@protonmail.ch>
2022-03-23 16:13:05 +0100

    fixed explosion loop

--- Assets/_Project/Units/Ally/Explosions/ExplosionBehaviour.cs
+++ Assets/_Project/Units/Ally/Explosions/ExplosionBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Kamikaze.Units.Ally.Explosions
 {
@@ -6,12 +7,19 @@ namespace Kamikaze.Units.Ally.Explosions
     /// Behaviour that handles explosions.
     /// Should be placed on the object where the explosion should happen (not on the explosion prefab).
     /// </summary>
+    [RequireComponent(typeof(ParticleSystem))]
     public class ExplosionBehaviour : MonoBehaviour
     {
         private float disableTimer;
+        private ParticleSystem particleSystem;
 
         public ExplosionScriptableObject ScriptableObject { get; set; }
         public ExplosionManager ExplosionManager { get; set; }
+
+        private void Awake()
+        {
+            particleSystem = GetComponent<ParticleSystem>();
+        }
 
         private void Update()
         {
@@ -29,6 +37,7 @@ namespace Kamikaze.Units.Ally.Explosions
             disableTimer = explosionScriptable.ExplosionTime;
             ScriptableObject = explosionScriptable;
             gameObject.SetActive(true);
+            particleSystem.Play(true);
         }
     }
 }

141c92852c4167bff03785cc7bcf465ca683c044
Stowy <stowy@protonmail.ch>
2022-03-24 14:15:57 +0100

    made enemy drop

---
+++ Assets/_Project/Units/Enemy/DropOnDeathBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Kamikaze.MonetarySystem;
+using UnityEngine;
+
+namespace Kamikaze.Units.Enemy
+{
+	[RequireComponent(typeof(HealthBehaviour))]
+	public class DropOnDeathBehaviour : MonoBehaviour
+	{
+		[SerializeField] private GameObject dropPrefab;
+
+		private HealthBehaviour healthBehaviour;
+		
+		public int DropAmount { get; set; }
+
+		private void Awake()
+		{
+			healthBehaviour = GetComponent<HealthBehaviour>();
+			healthBehaviour.OnHurt += OnHurt;
+		}
+
+		private void OnHurt(int healthPoints)
+		{
+			if (!healthBehaviour.IsAlive)
+			{
+				DropItem();
+			}
+		}
+
+		private void DropItem()
+		{
+			GameObject item = Instantiate(dropPrefab, transform.position, Quaternion.identity);
+			var dropBehaviour = item.GetComponent<DropBehaviour>();
+			dropBehaviour.DropAmount = DropAmount;
+		}
+	}
+}

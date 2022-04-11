3b3bb496d170f95dcd8394617f9c44070843d006
Stowy <stowy@protonmail.ch>
2022-04-11 12:08:41 +0200

    killed enemies at the end of a level

--- Assets/_Project/Units/HealthBehaviour.cs
+++ Assets/_Project/Units/HealthBehaviour.cs
@@ -4,57 +4,63 @@ using UnityEngine;
 
 namespace Kamikaze.Units
 {
-	/// <summary>
-	///     Script to put on game objects that must have health like the player or enemies.
-	/// </summary>
-	public class HealthBehaviour : MonoBehaviour
-	{
-		public delegate void HurtCallback(int healthPoints);
+    /// <summary>
+    ///     Script to put on game objects that must have health like the player or enemies.
+    /// </summary>
+    public class HealthBehaviour : MonoBehaviour
+    {
+        public delegate void HurtCallback(int healthPoints);
 
-		[SerializeField] private bool destroyWhenKilled = true;
+        [SerializeField] private bool destroyWhenKilled = true;
 
-		[ConditionalField(nameof(destroyWhenKilled))] [SerializeField]
-		private float destroyTime;
+        [ConditionalField(nameof(destroyWhenKilled))]
+        [SerializeField]
+        private float destroyTime;
 
-		[SerializeField] private SoundEffectScriptableObject deathSound;
+        [SerializeField] private SoundEffectScriptableObject deathSound;
 
-		public HurtCallback OnHurt { get; set; }
-		public bool IsAlive { get; private set; }
+        public HurtCallback OnHurt { get; set; }
+        public bool IsAlive { get; private set; }
 
-		[field: ReadOnly]
-		[field: SerializeField]
-		public int HealthPoints { get; set; }
+        [field: ReadOnly]
+        [field: SerializeField]
+        public int HealthPoints { get; set; }
 
-		public int MaxHealthPoints { get; set; } = 15;
+        public int MaxHealthPoints { get; set; } = 15;
 
-		private void Awake()
-		{
-			IsAlive = true;
-		}
+        private void Awake()
+        {
+            IsAlive = true;
+        }
 
-		private void Start()
-		{
-			// Set the health points to MaxHealthPoints
-			// Max health points should be set in an awake function
-			HealthPoints = MaxHealthPoints;
-		}
+        private void Start()
+        {
+            // Set the health points to MaxHealthPoints
+            // Max health points should be set in an awake function
+            HealthPoints = MaxHealthPoints;
+        }
 
-		/// <summary>
-		///     Reduces the health of the game object and kills it if health = 0.
-		/// </summary>
-		/// <param name="amount">The amount of health to subtract. Defaults to 1.</param>
-		public void ReduceHealth(int amount = 1)
-		{
-			HealthPoints -= amount;
+        /// <summary>
+        ///     Reduces the health of the game object and kills it if health = 0.
+        /// </summary>
+        /// <param name="amount">The amount of health to subtract. Defaults to 1.</param>
+        public void ReduceHealth(int amount = 1)
+        {
+            HealthPoints -= amount;
 
-			IsAlive = HealthPoints > 0;
+            IsAlive = HealthPoints > 0;
 
-			OnHurt?.Invoke(HealthPoints);
+            OnHurt?.Invoke(HealthPoints);
 
-			if (IsAlive || !destroyWhenKilled) return;
-			
-			if (deathSound != null) deathSound.Play();
-			Destroy(gameObject, destroyTime);
-		}
-	}
+            if (IsAlive || !destroyWhenKilled) return;
+
+            if (deathSound != null) deathSound.Play();
+            Destroy(gameObject, destroyTime);
+        }
+
+        public void Kill()
+        {
+            ReduceHealth(HealthPoints);
+        }
+    }
 }
--- Assets/_Project/Waves/WaveSpawner.cs
+++ Assets/_Project/Waves/WaveSpawner.cs
@@ -13,148 +13,153 @@ using Random = UnityEngine.Random;
 
 namespace Kamikaze.Waves
 {
-	public class WaveSpawner : MonoBehaviour
-	{
-		[SerializeField] private LevelsManagerScriptableObject levelsManager;
-		[SerializeField] private LanesManager lanesManager;
-		[SerializeField] private LifeManager lifeManager;
-		[SerializeField] private float waitTime;
-		[SerializeField] private Wave[] waves;
-		[SerializeField] private GameObject winText;
+    public class WaveSpawner : MonoBehaviour
+    {
+        [SerializeField] private LevelsManagerScriptableObject levelsManager;
+        [SerializeField] private LanesManager lanesManager;
+        [SerializeField] private LifeManager lifeManager;
+        [SerializeField] private float waitTime;
+        [SerializeField] private Wave[] waves;
+        [SerializeField] private GameObject winText;
 
-		[Tooltip("The time it takes to load the level menu after completing the level.")] [SerializeField]
-		private float timeToLoadLevelMenu;
+        [Tooltip("The time it takes to load the level menu after completing the level.")]
+        [SerializeField]
+        private float timeToLoadLevelMenu;
 
-		[SerializeField] private SceneReference levelMenuScene;
-		[SerializeField] private MoneyScriptableObject money;
-		[SerializeField] private DropManager dropManager;
+        [SerializeField] private SceneReference levelMenuScene;
+        [SerializeField] private MoneyScriptableObject money;
+        [SerializeField] private DropManager dropManager;
 
-		private int currentWaveIndex;
-		private bool isLevelOver;
+        private int currentWaveIndex;
+        private bool isLevelOver;
 
-		public Wave[] Waves => waves;
+        public Wave[] Waves => waves;
 
-		public Wave CurrentWave => Waves[currentWaveIndex];
+        public Wave CurrentWave => Waves[currentWaveIndex];
 
-		private void Start()
-		{
-			for (var i = 0; i < Waves.Length; i++)
-			{
-				Wave wave = Waves[i];
-				wave.waveState = currentWaveIndex == i ? WaveState.Active : WaveState.Inactive;
+        private void Start()
+        {
+            for (var i = 0; i < Waves.Length; i++)
+            {
+                Wave wave = Waves[i];
+                wave.waveState = currentWaveIndex == i ? WaveState.Active : WaveState.Inactive;
 
-				foreach (SpawnerData spawner in wave.spawners)
-				{
-					spawner.maxEnemies = spawner.noOfEnemiesToSpawn;
-					if (spawner.maxEnemies > 0) wave.totalEnemies += spawner.maxEnemies;
-				}
-			}
-		}
+                foreach (SpawnerData spawner in wave.spawners)
+                {
+                    spawner.maxEnemies = spawner.noOfEnemiesToSpawn;
+                    if (spawner.maxEnemies > 0) wave.totalEnemies += spawner.maxEnemies;
+                }
+            }
+        }
 
-		private void Update()
-		{
-			if (!isLevelOver) UpdateWaveState();
-		}
+        private void Update()
+        {
+            if (!isLevelOver) UpdateWaveState();
+        }
 
-		private void UpdateWaveState()
-		{
-			switch (CurrentWave.waveState)
-			{
-				case WaveState.Active:
-					SpawnWave();
-					if (CurrentWave.totalEnemies <= 0)
-					{
-						CurrentWave.waveState = WaveState.Inactive;
-						if (currentWaveIndex + 1 < Waves.Length)
-						{
-							currentWaveIndex++;
-							StartCoroutine(StartNextWaveCoroutine());
-						}
-					}
+        private void UpdateWaveState()
+        {
+            switch (CurrentWave.waveState)
+            {
+                case WaveState.Active:
+                    SpawnWave();
+                    if (CurrentWave.totalEnemies <= 0)
+                    {
+                        CurrentWave.waveState = WaveState.Inactive;
+                        if (currentWaveIndex + 1 < Waves.Length)
+                        {
+                            currentWaveIndex++;
+                            StartCoroutine(StartNextWaveCoroutine());
+                        }
+                    }
 
-					break;
-				case WaveState.Inactive:
-					if (CurrentWave.totalEnemies <= 0) CompleteLevel();
+                    break;
+                case WaveState.Inactive:
+                    if (CurrentWave.totalEnemies <= 0) CompleteLevel();
 
-					break;
-				default:
-					throw new ArgumentOutOfRangeException();
-			}
-		}
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
 
-		private void CompleteLevel()
-		{
-			this.Log("level completed!");
-			isLevelOver = true;
-			winText.SetActive(true);
+        private void CompleteLevel()
+        {
+            this.Log("level completed!");
+            isLevelOver = true;
+            winText.SetActive(true);
 
-			// Pickup all the remaining allies
-			var allies = FindObjectsOfType<BackInInventoryBehaviour>();
-			allies.ForEach(ally => ally.ReturnInInventory());
+            // Pickup all the remaining allies
+            var allies = FindObjectsOfType<BackInInventoryBehaviour>();
+            allies.ForEach(ally => ally.ReturnInInventory());
 
-			// Pickup the gems
-			var gems = FindObjectsOfType<DropBehaviour>();
-			gems.ForEach(gem => dropManager.PickupGem(gem));
+            // Kill all enemies
+            var enemies = FindObjectsOfType<HealthBehaviour>();
+            enemies.ForEach(enemy => enemy.Kill());
 
-			// Add the gems to the global money
-			money.AddGems(dropManager);
+            // Pickup the gems
+            var gems = FindObjectsOfType<DropBehaviour>();
+            gems.ForEach(gem => dropManager.PickupGem(gem));
 
-			// Stop the placement of units
-			foreach (Lane lane in lanesManager.Lanes)
-			{
-				lane.tag = "Lane";
-			}
-			
-			// Set this level to done
-			levelsManager.FinishCurrentLevel();
+            // Add the gems to the global money
+            money.AddGems(dropManager);
 
-			StartCoroutine(LoadLevelMenuCoroutine());
-		}
+            // Stop the placement of units
+            foreach (Lane lane in lanesManager.Lanes)
+            {
+                lane.tag = "Lane";
+            }
 
-		private void SpawnWave()
-		{
-			foreach (SpawnerData spawner in CurrentWave.spawners)
-			{
-				if (!spawner.canSpawnEnemy || !(spawner.nextSpawnTime < Time.time)) continue;
+            // Set this level to done
+            levelsManager.FinishCurrentLevel();
 
-				//Instantiates random enemy types
-				int enemyTypeIndex = Random.Range(0, spawner.typesOfEnemies.Length);
-				GameObject enemyType = spawner.typesOfEnemies[enemyTypeIndex].gameObject;
-				GameObject enemy = Instantiate(enemyType, transform.position, Quaternion.identity);
+            StartCoroutine(LoadLevelMenuCoroutine());
+        }
 
-				//Assign its position
-				var enemyLaneUnitBehavior = enemy.GetComponent<LaneUnitBehaviour>();
-				enemyLaneUnitBehavior.LaneId = spawner.laneId;
-				enemyLaneUnitBehavior.LanesManager = lanesManager;
+        private void SpawnWave()
+        {
+            foreach (SpawnerData spawner in CurrentWave.spawners)
+            {
+                if (!spawner.canSpawnEnemy || !(spawner.nextSpawnTime < Time.time)) continue;
 
-				//Links to wave spawner
-				var linkEnemyWaveSpawner = enemy.GetComponent<LinkEnemyWaveSpawner>();
-				linkEnemyWaveSpawner.WaveSpawner = this;
+                //Instantiates random enemy types
+                int enemyTypeIndex = Random.Range(0, spawner.typesOfEnemies.Length);
+                GameObject enemyType = spawner.typesOfEnemies[enemyTypeIndex].gameObject;
+                GameObject enemy = Instantiate(enemyType, transform.position, Quaternion.identity);
 
-				//Links to life manager
-				var reducePlayerLife = enemy.GetComponent<ReducePlayerLife>();
-				reducePlayerLife.LifeManager = lifeManager;
+                //Assign its position
+                var enemyLaneUnitBehavior = enemy.GetComponent<LaneUnitBehaviour>();
+                enemyLaneUnitBehavior.LaneId = spawner.laneId;
+                enemyLaneUnitBehavior.LanesManager = lanesManager;
 
-				// Link wave id to ReduceWaveTotalEnemies
-				var reduceWaveTotalEnemies = enemy.GetComponent<ReduceWaveTotalEnemies>();
-				reduceWaveTotalEnemies.WaveId = currentWaveIndex;
+                //Links to wave spawner
+                var linkEnemyWaveSpawner = enemy.GetComponent<LinkEnemyWaveSpawner>();
+                linkEnemyWaveSpawner.WaveSpawner = this;
 
-				spawner.noOfEnemiesToSpawn--;
-				spawner.nextSpawnTime = Time.time + spawner.spawnInterval;
-				if (spawner.noOfEnemiesToSpawn == 0) spawner.canSpawnEnemy = false;
-			}
-		}
+                //Links to life manager
+                var reducePlayerLife = enemy.GetComponent<ReducePlayerLife>();
+                reducePlayerLife.LifeManager = lifeManager;
 
-		public IEnumerator StartNextWaveCoroutine()
-		{
-			yield return new WaitForSeconds(waitTime);
-			CurrentWave.waveState = WaveState.Active;
-		}
+                // Link wave id to ReduceWaveTotalEnemies
+                var reduceWaveTotalEnemies = enemy.GetComponent<ReduceWaveTotalEnemies>();
+                reduceWaveTotalEnemies.WaveId = currentWaveIndex;
 
-		public IEnumerator LoadLevelMenuCoroutine()
-		{
-			yield return new WaitForSeconds(timeToLoadLevelMenu);
-			levelMenuScene.LoadScene();
-		}
-	}
+                spawner.noOfEnemiesToSpawn--;
+                spawner.nextSpawnTime = Time.time + spawner.spawnInterval;
+                if (spawner.noOfEnemiesToSpawn == 0) spawner.canSpawnEnemy = false;
+            }
+        }
+
+        public IEnumerator StartNextWaveCoroutine()
+        {
+            yield return new WaitForSeconds(waitTime);
+            CurrentWave.waveState = WaveState.Active;
+        }
+
+        public IEnumerator LoadLevelMenuCoroutine()
+        {
+            yield return new WaitForSeconds(timeToLoadLevelMenu);
+            levelMenuScene.LoadScene();
+        }
+    }
 }

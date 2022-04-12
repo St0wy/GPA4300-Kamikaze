9d8298acfafbf013d56f9da59a8e3dcb52b0a73e
Stowy <stowy@protonmail.ch>
2022-04-12 16:48:19 +0200

    added a script to take screenshots during developpement builds

---
+++ Assets/_Project/Scripts/Screenshot.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using StowyTools.Logger;
+using UnityEngine;
+
+namespace Kamikaze
+{
+	public class Screenshot : MonoBehaviour
+	{
+		[Tooltip("The time it takes in seconds to take a screenshot.")] [SerializeField]
+		private float screenshotTime;
+
+		private float timer;
+		private string screenshotPath;
+
+		private void Awake()
+		{
+			DontDestroyOnLoad(this);
+			screenshotPath = Path.Combine(Application.dataPath, @"..\screenshots\");
+		}
+
+		private void Start()
+		{
+			if (!Directory.Exists(screenshotPath))
+			{
+				Directory.CreateDirectory(screenshotPath);
+			}
+		}
+
+		private void Update()
+		{
+			if (!Debug.isDebugBuild) return;
+
+			timer += Time.deltaTime;
+			if (!(timer >= screenshotTime)) return;
+
+			timer -= screenshotTime;
+			TakeScreenshot();
+		}
+
+		public void TakeScreenshot()
+		{
+			DateTime now = DateTime.Now;
+			string path = Path.Combine(screenshotPath, $"./kamikaze_{now:yyyy-MM-dd_hh-mm-ss}.png");
+			ScreenCapture.CaptureScreenshot(path);
+		}
+	}
+}
